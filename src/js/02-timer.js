103d3fd68a1782d230879215e6d904a14042111b
sergey-g80 <119445810+sergey-g80@users.noreply.github.com>
2023-05-14 14:46:25 +0300

    st6

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
 import flatpickr from 'flatpickr';
-// import convertMs from './dateConvert';
+import convertMs from './dateConvert';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 // Import additional css styles
@@ -99,21 +99,21 @@ function renderDate(formatDate) {
 
 // convertMs
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours const
-  hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours const
+//   hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  return { days, hours, minutes, seconds };
-}
+//   return { days, hours, minutes, seconds };
+// }
